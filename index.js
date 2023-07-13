aaff0a586cf2dc1ad2872199af46577818466bc3
williamNyongesa <marakawilliam@gmail.com>
2023-07-13 08:45:07 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,16 @@
+function receivesAFunction(callback){
+    callback();    
+}
+function returnsANamedFunction() {
+  
+    function namedFunction() {
+     
+    }
+    return namedFunction;
+  }
+
+  function returnsAnAnonymousFunction() {
+    return function(){
+        
+    }
+  }
